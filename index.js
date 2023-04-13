1816a00c1327e4efe3a660ad029724937df96b34
Junaid Ahmed <junaid.ahmed@postnord.com>
2023-04-13 23:59:11 +0200

    fixing the victory message

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if(randomNumber1<randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 WON! 🚩";
-}else if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML = "Player 2 WON! 🚩";
+}else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 WON! 🚩";
 }else{
     document.querySelector("h1").innerHTML = "DRAW! Roll Again.";
 }
